1f81125a08c7f1927778171d27197fd61fd9ede7
Sam Leveugle <leveuglesam98@gmail.com>
2026-09-16 20:05:34 +0200

    Add responsive header menu.

--- web/themes/custom/drupal_test/js/header.js
+++ web/themes/custom/drupal_test/js/header.js
@@ -1,25 +1,34 @@
 (function (Drupal, once) {
-    'use strict';
-  
-    Drupal.behaviors.drupalTestHeaderScroll = {
-      attach: function (context) {
-        once('drupal-test-header-scroll', 'html', context).forEach(function () {
-          var header = document.querySelector('.header');
-          if (!header) {
-            return;
+  "use strict";
+
+  Drupal.behaviors.drupalTestHeaderScroll = {
+    attach: function (context) {
+      once("drupal-test-header-scroll", "html", context).forEach(function () {
+        var header = document.querySelector(".header");
+        if (!header) {
+          return;
+        }
+
+        var onScroll = function () {
+          if (window.scrollY > 20) {
+            header.classList.add("header--scrolled");
+          } else {
+            header.classList.remove("header--scrolled");
           }
-  
-          var onScroll = function () {
-            if (window.scrollY > 20) {
-              header.classList.add('header--scrolled');
-            } else {
-              header.classList.remove('header--scrolled');
-            }
-          };
-  
-          window.addEventListener('scroll', onScroll, { passive: true });
-          onScroll();
-        });
-      },
-    };
-  })(Drupal, once);
+        };
+
+        var toggle = header.querySelector(".header__toggle");
+        var menu = header.querySelector(".header__menu");
+        if (toggle && menu) {
+          toggle.addEventListener("click", function () {
+            var open = header.classList.toggle("header--open");
+            toggle.setAttribute("aria-expanded", open ? "true" : "false");
+          });
+        }
+
+        window.addEventListener("scroll", onScroll, { passive: true });
+        onScroll();
+      });
+    },
+  };
+})(Drupal, once);
